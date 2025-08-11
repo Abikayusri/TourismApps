cfdcddbcdec194cbb11572ed43b868bedfccf1e3
Abikayusri <abikayusri96@gmail.com>
2025-08-12 02:26:11 +0700

    [Exercise-7] Add connect to API with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tourism_app/data/api/api_services.dart';
 import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
 import 'package:tourism_app/provider/main/index_nav_provider.dart';
 import 'package:tourism_app/screen/detail/detail_screen.dart';
 import 'package:tourism_app/screen/main/main_screen.dart';
-import 'package:tourism_app/static/navigation_route.dart';
+import 'package:tourism_app/static/navigation/navigation_route.dart';
 import 'package:tourism_app/style/theme/tourism_theme.dart';
+
+import 'provider/detail/tourism_detail_provider.dart';
+import 'provider/home/tourism_list_provider.dart';
 
 void main() {
   runApp(
@@ -16,6 +20,19 @@ void main() {
         ),
         ChangeNotifierProvider(
           create: (context) => BookmarkListProvider(),
+        ),
+        Provider(
+          create: (context) => ApiServices(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TourismListProvider(
+            context.read<ApiServices>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TourismDetailProvider(
+            context.read<ApiServices>(),
+          ),
         ),
       ],
       child: const MyApp(),
