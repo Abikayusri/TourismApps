3834de607d8c176ee41dda308fb22e8818bd5f50
Abikayusri <abikayusri96@gmail.com>
2025-08-22 04:54:40 +0700

    [Exercise-8] Add feature bookmarkø

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tourism_app/data/api/api_services.dart';
-import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
+import 'package:tourism_app/data/local/local_database_service.dart';
+import 'package:tourism_app/provider/bookmark/local_database_provider.dart';
 import 'package:tourism_app/provider/main/index_nav_provider.dart';
 import 'package:tourism_app/screen/detail/detail_screen.dart';
 import 'package:tourism_app/screen/main/main_screen.dart';
@@ -15,24 +16,19 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => IndexNavProvider()),
+        Provider(create: (context) => ApiServices()),
         ChangeNotifierProvider(
-          create: (context) => IndexNavProvider(),
+          create: (context) => TourismListProvider(context.read<ApiServices>()),
         ),
         ChangeNotifierProvider(
-          create: (context) => BookmarkListProvider(),
+          create: (context) =>
+              TourismDetailProvider(context.read<ApiServices>()),
         ),
-        Provider(
-          create: (context) => ApiServices(),
-        ),
+        Provider(create: (context) => LocalDatabaseService()),
         ChangeNotifierProvider(
-          create: (context) => TourismListProvider(
-            context.read<ApiServices>(),
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => TourismDetailProvider(
-            context.read<ApiServices>(),
-          ),
+          create: (context) =>
+              LocalDatabaseProvider(context.read<LocalDatabaseService>()),
         ),
       ],
       child: const MyApp(),
@@ -54,8 +50,8 @@ class MyApp extends StatelessWidget {
       routes: {
         NavigationRoute.mainRoute.name: (context) => const MainScreen(),
         NavigationRoute.detailRoute.name: (context) => DetailScreen(
-              tourismId: ModalRoute.of(context)?.settings.arguments as int,
-            ),
+          tourismId: ModalRoute.of(context)?.settings.arguments as int,
+        ),
       },
     );
   }
--- lib/screen/detail/bookmark_icon_widget.dart
+++ lib/screen/detail/bookmark_icon_widget.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tourism_app/data/model/tourism.dart';
-import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
+import 'package:tourism_app/provider/bookmark/local_database_provider.dart';
 import 'package:tourism_app/provider/detail/bookmark_icon_provider.dart';
 
 class BookmarkIconWidget extends StatefulWidget {
   final Tourism tourism;
 
-  const BookmarkIconWidget({
-    super.key,
-    required this.tourism,
-  });
+  const BookmarkIconWidget({super.key, required this.tourism});
 
   @override
   State<BookmarkIconWidget> createState() => _BookmarkIconWidgetState();
@@ -19,13 +16,13 @@ class BookmarkIconWidget extends StatefulWidget {
 class _BookmarkIconWidgetState extends State<BookmarkIconWidget> {
   @override
   void initState() {
-    final bookmarkListProvider = context.read<BookmarkListProvider>();
+    final localDatabaseProvider = context.read<LocalDatabaseProvider>();
     final bookmarkIconProvider = context.read<BookmarkIconProvider>();
 
-    Future.microtask(() {
-      final tourismInList =
-          bookmarkListProvider.checkItemBookmark(widget.tourism);
-      bookmarkIconProvider.isBookmarked = tourismInList;
+    Future.microtask(() async {
+      await localDatabaseProvider.loadTourismById(widget.tourism.id);
+      final value = localDatabaseProvider.checkItemBookmark(widget.tourism.id);
+      bookmarkIconProvider.isBookmarked = value;
     });
 
     super.initState();
@@ -34,17 +31,18 @@ class _BookmarkIconWidgetState extends State<BookmarkIconWidget> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        final bookmarkListProvider = context.read<BookmarkListProvider>();
+      onPressed: () async {
+        final localDatabaseProvider = context.read<LocalDatabaseProvider>();
         final bookmarkIconProvider = context.read<BookmarkIconProvider>();
         final isBookmarked = bookmarkIconProvider.isBookmarked;
 
-        if (isBookmarked) {
-          bookmarkListProvider.removeBookmark(widget.tourism);
+        if (!isBookmarked) {
+          await localDatabaseProvider.saveTourism(widget.tourism);
         } else {
-          bookmarkListProvider.addBookmark(widget.tourism);
+          await localDatabaseProvider.removeTourismById(widget.tourism.id);
         }
-        context.read<BookmarkIconProvider>().isBookmarked = !isBookmarked;
+        bookmarkIconProvider.isBookmarked = !isBookmarked;
+        localDatabaseProvider.loadAllTourism();
       },
       icon: Icon(
         context.watch<BookmarkIconProvider>().isBookmarked
